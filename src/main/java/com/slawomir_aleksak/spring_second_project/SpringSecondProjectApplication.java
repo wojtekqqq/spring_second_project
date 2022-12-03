3d46027dc4c2e8adb59e8a64701b6787ed0907b6
saleksak <wojtekqq@tlen.pl>
2022-12-03 14:04:00 +0100

    start

---
+++ src/main/java/com/slawomir_aleksak/spring_second_project/SpringSecondProjectApplication.java
@@ -0,0 +1,13 @@
+package com.slawomir_aleksak.spring_second_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecondProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecondProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slawomir_aleksak/spring_second_project/SpringSecondProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slawomir_aleksak.spring_second_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecondProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
